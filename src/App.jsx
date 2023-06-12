b1e1c39a353773f0a0e11718a9b41a6253594a27
Adewoye Emmanuel <89729017+Emmaxfans@users.noreply.github.com>
2023-06-12 14:10:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/landing/Landing';
 import Dashboard from './pages/dashboard/Dashboard';
-import Signin from './pages/sign-in/signin';
+// import Signin from './pages/sign-in/signin';
 import Signup from './pages/sign-up/signup';
 import Error from './pages/error/error';
 
