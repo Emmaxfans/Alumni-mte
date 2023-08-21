180acd07f18dbac7a6d256860899a704c0112e69
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-08-21 22:28:27 +0100

    updating image components of forgetten password pages

--- src/components/forgot-password/Backbutton.jsx
+++ src/components/forgot-password/Backbutton.jsx
@@ -3,6 +3,7 @@ import backbutton from "../../assets/icons/backbutton.png";
 
 const Backbutton = () => {
   return (
+    // this back button icon
     <div className="backbutton">
       <img src={backbutton} alt="back-button" />
     </div>
