0caa3b4c708b3f657a85847ef673adadea9689a7
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-08-21 22:11:40 +0100

    updating image components of forgetten password pages

--- src/components/forgot-password/Bottomcircle.jsx
+++ src/components/forgot-password/Bottomcircle.jsx
@@ -1,11 +1,11 @@
 import "./Bottomcircle.css";
-import ellipse1 from "../../assets/images/forgotpsw/ellipse37.png";
+// import ellipse1 from "../../assets/images/forgotpsw/ellipse37.png";
 
 const Bottomcircle = () => {
   return (
     <>
       <div className="fp__div1">
-        <img src={ellipse1} alt="ellips1" />
+        {/* <img src={ellipse1} alt="ellips1" /> */}
       </div>
     </>
   );
