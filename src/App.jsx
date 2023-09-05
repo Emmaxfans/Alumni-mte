054128856cb668a8242b931b8f6442a74063fb31
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-09-05 10:50:03 +0100

    updating not found page

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Signin from "./pages/sign-in/Signin";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Signup from "./pages/sign-up/Signup";
 import Gallery from "./pages/gallery/Gallery";
-import ForgotPassword from "./pages/forgot-password/password-reset/ForgotPassword";
-import EmailReset from "./pages/forgot-password/email-reset/EmailReset";
-import SucessReset from "./pages/forgot-password/sucess-reset/SucessReset";
-import ResetInput from "./pages/forgot-password/password-reset-input/ResetInput";
-import LoginUpsideLoginForm from "./pages/formvalidation/LoginUpsideLoginForm";
+// import ForgotPassword from "./pages/forgot-password/password-reset/ForgotPassword";
+// import EmailReset from "./pages/forgot-password/email-reset/EmailReset";
+// import SucessReset from "./pages/forgot-password/sucess-reset/SucessReset";
+// import ResetInput from "./pages/forgot-password/password-reset-input/ResetInput";
+// import LoginUpsideLoginForm from "./pages/formvalidation/LoginUpsideLoginForm";
 import Error from "./pages/error/Error";
 // import FormTest from './pages/formvalidation/formTest';
 // import FormValidate from './pages/formvalidation/FormValidate';
@@ -27,11 +27,11 @@ function App() {
           <Route path="*" element={<Error />} />
 
           {/* forgot password  */}
-          <Route path="/forgotpsw" element={<ForgotPassword />} />
-          <Route path="/resetinput" element={<ResetInput />} />
-          <Route path="/emailreset" element={<EmailReset />} />
-          <Route path="/resetpsw" element={<SucessReset />} />
-          <Route path="/form" element={<LoginUpsideLoginForm />} />
+          {/* <Route path="/forgotpsw" element={<ForgotPassword />} /> */}
+          {/* <Route path="/resetinput" element={<ResetInput />} /> */}
+          {/* <Route path="/emailreset" element={<EmailReset />} /> */}
+          {/* <Route path="/resetpsw" element={<SucessReset />} /> */}
+          {/* <Route path="/form" element={<LoginUpsideLoginForm />} /> */}
           {/* <Route path='/form1' element={<FormTest />} /> */}
         </Routes>
       </Router>
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -2,7 +2,10 @@ import "./Error.css";
 const Error = () => {
   return (
     <div className="error-page">
-      <h1>This page does not exist</h1>
+      <h1>
+        This page does not exist, click
+        <a href="/"> here </a> to go back to homepage
+      </h1>
     </div>
   );
 };
